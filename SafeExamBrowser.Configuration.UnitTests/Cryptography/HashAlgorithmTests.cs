273e404f60a629fe532c28b524fd7fb24d600acc
dbuechel <damian.buechel@let.ethz.ch>
2019-02-15 10:11:31 +0100

    SEBWIN-296: Implemented unit tests for hash algorithm.

---
+++ SafeExamBrowser.Configuration.UnitTests/Cryptography/HashAlgorithmTests.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Copyright (c) 2019 ETH Zürich, Educational Development and Technology (LET)
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SafeExamBrowser.Configuration.Cryptography;
+
+namespace SafeExamBrowser.Configuration.UnitTests.Cryptography
+{
+	[TestClass]
+	public class HashAlgorithmTests
+	{
+		private HashAlgorithm sut;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			sut = new HashAlgorithm();
+		}
+
+		[TestMethod]
+		public void MustGeneratePasswordHashCorrectly()
+		{
+			var hash = "4adfa806cb610693a6200e4cdbdafeaf352876a35f964a781d691457df9cd378";
+			var generated = sut.GenerateHashFor("blabbedyblubbedy");
+
+			Assert.AreEqual(hash, generated);
+		}
+	}
+}
