0d270b1444d24feeed31555cb464e41e24e4a99f
dbuechel <damian.buechel@let.ethz.ch>
2019-03-29 10:52:00 +0100

    SEBWIN-226: Trying to use CI to set correct version and build number for assemblies.

--- SafeExamBrowser.Runtime/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Runtime/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0 (alpha)")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
