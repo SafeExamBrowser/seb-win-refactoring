6cfa5f06c8a0f6bb0c3c9f0a075a814f94f603df
dbuechel <damian.buechel@let.ethz.ch>
2019-08-07 15:51:15 +0200

    SEBWIN-337: Fixed assembly info for legacy configuration tool.

--- SebWindowsConfig/Properties/AssemblyInfo.cs
+++ SebWindowsConfig/Properties/AssemblyInfo.cs
@@ -1,18 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SEB Config Tool")]
-[assembly: AssemblyDescription("SEB Configuration Tool")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ETH Zurich")]
-[assembly: AssemblyProduct("SEB Config Tool")]
-[assembly: AssemblyCopyright("Copyright © 2010-2019 ETH Zurich, Educational Development and Technology (LET)")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("SEB Configuration Tool")]
+[assembly: AssemblyDescription("Safe Exam Browser")]
+[assembly: AssemblyCompany("ETH Zürich")]
+[assembly: AssemblyProduct("SEB Configuration Tool")]
+[assembly: AssemblyCopyright("Copyright © 2019 ETH Zürich, Educational Development and Technology (LET)")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +28,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.*")]
-[assembly: AssemblyInformationalVersion("2.2.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
