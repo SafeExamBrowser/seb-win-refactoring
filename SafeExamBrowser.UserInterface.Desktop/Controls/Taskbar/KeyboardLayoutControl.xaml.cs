20a2c1192794debce4a30971706f3801fee77ddc
Jonas Sourlier <jonas.sourlier@yolp.ch>
2022-09-22 11:28:33 +0200

    set tooltip help text for JAWS

--- SafeExamBrowser.UserInterface.Desktop/Controls/Taskbar/KeyboardLayoutControl.xaml.cs
+++ SafeExamBrowser.UserInterface.Desktop/Controls/Taskbar/KeyboardLayoutControl.xaml.cs
@@ -10,6 +10,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
@@ -120,6 +121,7 @@ namespace SafeExamBrowser.UserInterface.Desktop.Controls.Taskbar
 
 			LayoutCultureCode.Text = layout.CultureCode;
 			Button.ToolTip = tooltip;
+			AutomationProperties.SetHelpText(Button, tooltip);
 		}
 
 		private void Popup_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
