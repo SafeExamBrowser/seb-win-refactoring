9400057c1b55ee3b13d8d21db921ebf6eed3c234
Damian Büchel <damian.buechel@id.ethz.ch>
2026-03-03 15:12:20 +0100

    Updated year in license and copyright remarks. Added QRCoder to license and version information.

--- SafeExamBrowser.Browser/Content/PageZoom.js
+++ SafeExamBrowser.Browser/Content/PageZoom.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 ETH Zürich, IT Services
+ * Copyright (c) 2026 ETH Zürich, IT Services
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
